b85a1bd14de74fbbffdf99ee86bfdf9a40819686
Pesp05 <mendez.espab24@triana.salesianos.edu>
2025-01-27 10:55:47 +0100

    SpringBoot Project Created

---
+++ conectapp/src/main/java/com/salesianostriana/dam/conectapp/ConectappApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.conectapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectappApplication.class, args);
+	}
+
+}
