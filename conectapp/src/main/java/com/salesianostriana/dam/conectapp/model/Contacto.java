bd1e32329c196a7ee2123b5cd45f2d905cdc3559
Zaid <pilarads06@gmail.com>
2025-01-27 22:08:39 +0100

    Clase Contacto y ContactoPK con su relacion

--- conectapp/src/main/java/com/salesianostriana/dam/conectapp/model/Contacto.java
+++ conectapp/src/main/java/com/salesianostriana/dam/conectapp/model/Contacto.java
@@ -2,9 +2,11 @@ package com.salesianostriana.dam.conectapp.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Getter
@@ -15,12 +17,47 @@ import java.util.List;
 @NoArgsConstructor
 public class Contacto {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @EmbeddedId
+    private ContactoPK contactoPK;
+
     private String resumen;
     private String canal;
     private Date fecha;
 
-    @ManyToMany
-    private List<Profesor> listaProfesores;
+    @ManyToOne(fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL)
+    @MapsId("profesorId")
+    @JoinColumn(name = "profesor_id")
+    private Profesor profesor;
+
+    @ManyToOne(fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL)
+    @MapsId("trabajadorId")
+    @JoinColumn(name = "trabajador_id")
+    private Trabajador trabajador;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Contacto contacto = (Contacto) o;
+        return getContactoPK() != null && Objects.equals(getContactoPK(), contacto.getContactoPK());
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hash(contactoPK);
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "(" +
+                "EmbeddedId = " + contactoPK + ", " +
+                "resumen = " + resumen + ", " +
+                "canal = " + canal + ", " +
+                "fecha = " + fecha + ")";
+    }
 }
