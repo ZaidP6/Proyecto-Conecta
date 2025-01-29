ba40d1f9a6b0e46daa9516fd18a44e54f83b0bf6
ZaidP6 <pilarads06@gmail.com>
2025-01-29 12:20:32 +0100

    metodo controlador crearProfesor

--- conectapp/src/main/java/com/salesianostriana/dam/conectapp/controller/ProfesorController.java
+++ conectapp/src/main/java/com/salesianostriana/dam/conectapp/controller/ProfesorController.java
@@ -1,7 +1,12 @@
 package com.salesianostriana.dam.conectapp.controller;
 
+import com.salesianostriana.dam.conectapp.dto.ProfesorDto;
 import com.salesianostriana.dam.conectapp.service.ProfesorService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +17,9 @@ public class ProfesorController {
 
     private final ProfesorService profesorService;
 
-    
+    @PostMapping()
+    public ResponseEntity<ProfesorDto> crearProfesor(@RequestBody ProfesorDto nuevoProfe){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(profesorService.addNew(nuevoProfe));
+    }
 }
