5ee361a023138d5c0dbdd064ba5b7d78f2d13d95
Pesp05 <mendez.espab24@triana.salesianos.edu>
2025-02-03 13:11:02 +0100

    Endpoint funcional

--- conectapp/src/main/java/com/salesianostriana/dam/conectapp/ConectappApplication.java
+++ conectapp/src/main/java/com/salesianostriana/dam/conectapp/ConectappApplication.java
@@ -2,8 +2,18 @@ package com.salesianostriana.dam.conectapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =
+@Info(description = "API de la aplicación Conectapp para la gestión de y distribución de prácticas en empresa para alumnos de FP",
+		version = "1.0",
+		title = "API Conectapp"
+)
+)
 public class ConectappApplication {
 
 	public static void main(String[] args) {
