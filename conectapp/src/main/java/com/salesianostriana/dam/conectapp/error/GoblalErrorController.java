b6c65771c8be9a5b0eb1ac6643fd4200cd1eb178
Pesp05 <mendez.espab24@triana.salesianos.edu>
2025-02-04 12:27:57 +0100

    GoblalErrorController y EmpresaNotFoundException creados

---
+++ conectapp/src/main/java/com/salesianostriana/dam/conectapp/error/GoblalErrorController.java
@@ -0,0 +1,19 @@
+package com.salesianostriana.dam.conectapp.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GoblalErrorController extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(EmpresaNotFoundException.class)
+    public ProblemDetail handleEmpresaNotFound(EmpresaNotFoundException ex){
+        ProblemDetail result = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+        result.setTitle("Empresa no encontrada");
+        return result;
+    }
+
+}
