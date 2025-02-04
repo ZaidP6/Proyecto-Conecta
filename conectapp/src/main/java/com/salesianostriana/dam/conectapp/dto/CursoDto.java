c91c99bb5e55c0aa4669a978b206170491584fd4
ZaidP6 <pilarads06@gmail.com>
2025-02-04 12:23:16 +0100

    añadir curso ok

--- conectapp/src/main/java/com/salesianostriana/dam/conectapp/dto/CursoDto.java
+++ conectapp/src/main/java/com/salesianostriana/dam/conectapp/dto/CursoDto.java
@@ -3,15 +3,13 @@ package com.salesianostriana.dam.conectapp.dto;
 import com.salesianostriana.dam.conectapp.model.Curso;
 
 public record CursoDto(
-        Long id,
         String nombre,
         int horasEmpresa
 ) {
-//    public static GetCursoDto of(Curso curso) {
-//        return new GetCursoDto(
-//                curso.getId(),
-//                curso.getNombre(),
-//                curso.getHorasEmpresa()
-//        );
-//    }
+    public Curso toCurso() {
+        return Curso.builder()
+                .nombre(nombre)
+                .horasEmpresa(horasEmpresa)
+                .build();
+    }
 }
