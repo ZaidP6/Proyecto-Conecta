52936f991e33b5835d92b220c6a78726a686da02
ZaidP6 <pilarads06@gmail.com>
2025-02-11 10:22:32 +0100

    el principio de la seguridad

---
+++ conectapp/src/main/java/com/salesianostriana/dam/conectapp/security/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.salesianostriana.dam.conectapp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.httpBasic(Customizer.withDefaults());
+        http.authorizeHttpRequests((auth) -> auth
+                .requestMatchers(HttpMethod.POST,"/usuario/").hasRole("ADMIN")
+                .anyRequest().authenticated()
+        );
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withUsername("admin")
+                .password("{noop}1234")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}admin")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+}
