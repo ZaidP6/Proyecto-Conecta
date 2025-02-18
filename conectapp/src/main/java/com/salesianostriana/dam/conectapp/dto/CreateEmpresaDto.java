43d973a8e53c6679c4a1fe530fc7fc80aea16f24
Pesp05 <mendez.espab24@triana.salesianos.edu>
2025-02-18 14:29:33 +0100

    Cambios 18/02/25

--- conectapp/src/main/java/com/salesianostriana/dam/conectapp/dto/CreateEmpresaDto.java
+++ conectapp/src/main/java/com/salesianostriana/dam/conectapp/dto/CreateEmpresaDto.java
@@ -1,11 +1,19 @@
 package com.salesianostriana.dam.conectapp.dto;
 
 import com.salesianostriana.dam.conectapp.model.Empresa;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
 
 public record CreateEmpresaDto(
+        @NotBlank
         String cif,
+        @NotBlank
         String direccion,
+        @DecimalMin("0")
+        @DecimalMax("180")
         String coordenadas,
+        @NotBlank
         String nombre
 ) {
 
